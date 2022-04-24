4af04563dcc99423aafbbc65c3555f1e2e5c7162
saanikajoshi <spj29@njit.edu>
2022-04-24 14:48:58 -0400

    Card added

---
+++ src/Templates/Card.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import './css/style.css';
+
+const onCardAction = (taskId, column, callbackFn) => {
+    return () => {
+        callbackFn(taskId, column);
+    };
+}
+
+const renderButton = (taskId, column, btnText, callbackFn) => {
+    if (btnText && callbackFn) {
+        return (
+            <div className="m-2">
+                <button
+                    className="btn btn-outline-primary"
+                    onClick={onCardAction(taskId, column, callbackFn)}>
+                    { btnText }
+                </button>
+            </div>
+        );
+    } 
+    else {
+        return <span />;
+    }
+}
+
+const Card = props => {
+    return (
+        <div className="behind">
+            <div className="card">
+                <div className="card-body">
+                    <h4 className="card-title">
+                        { props.title }
+                    </h4>
+                    <div className="card-text">
+                        <div>ID: { props.id }</div>
+                        <div>Type: { props.type }</div>
+                        { renderButton(props.id, props.column, props.prevTxt, props.onPrevClick) }
+                        { renderButton(props.id, props.column, props.nextTxt, props.onNextClick) }
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
