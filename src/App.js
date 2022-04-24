4059a5836c24d67cd1ab2b759da84383a536cfbd
saanikajoshi <spj29@njit.edu>
2022-04-24 15:43:40 -0400

    mod app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import axios from 'axios';
+import Add from './Templates/Add';
+import Page from './Templates/Page';
+import View from './Templates/View'; 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const mobileWidth = 768;
+
+class App extends  React.Component {
+  state = {
+    view: 'grid',
+    allTasks: [],
+    sortedTasks: {
+      todo: [],
+      inProgress: [],
+      review: [],
+      done: []
+    },
+    errorText: ''
+  }
+  
+  getTasks() {
+    axios.get('https://my-json-server.typicode.com/saphire001/IS-322-102-2/posts')
+        .then(response => {
+          this.setState({ allTasks: response.data, sortedTasks: this.sortTasks(response.data) });
+        }).catch (error => {
+      this.setState({ errorMessage: error.message });
+    });
+  }
+
+  sortTasks(tasks) {
+    return {
+      todo: tasks.filter(post => post.column === 'todo'),
+      inProgress: tasks.filter(post => post.column === 'in-progress'),
+      review: tasks.filter(post => post.column === 'review'),
+      done: tasks.filter(post => post.column === 'done'),
+    }
+  } 
+
+  componentDidMount() {
+    this.getTasks();
+  }
+
+  wrapPage(jsx) {
+    const { view } = this.state;
+    return (
+      <div className="container">
+        <PageTabs currentView={view} onViewChange={this.onViewChange.bind(this)}/>
+        {jsx}
+      </div>
+    );
+  }
+
+  handleResize = () => {
+    const browserWidth = window.innerWidth;
+    let breakpoint = 'computer';
+    if (browserWidth < mobileWidth) {
+      breakpoint = 'mobile';
+    }
+    this.setState({ breakpoint, browserWidth });
+  }
+
+  onUpdateTask(_task){
+    let allTasks = this.state.allTasks;
+    const index = allTasks.findIndex(task => task.id === _task.id);
+    allTasks[index] = _task;
+    const sortedTasks = this.sortTasks(allTasks);
+    this.setState({ allTasks, sortedTasks })
+  }
+
+  onViewChange(view) {
+    this.setState({ view });
+  }
+
+  onAddTask(task) {
+    let tasks = this.state.allTasks;
+    task.column = 'todo';
+    task.id = this.state.allTasks.length + 1;
+    tasks.push(task);
+    let sortedTasks = this.sortTasks(tasks);
+    this.setState({ tasks, sortedTasks, view: 'grid' });
+  }
+
+  render(){
+    if (this.state.view === 'grid'){
+      return (this.wrapPage(<GridView tasks={this.state.sortedTasks} onUpdateTask={(task)=> this.onUpdateTask(task)} />));
+    }
+    else if (this.state.view === 'add') {
+      return (this.wrapPage(<AddTask tasks={this.state.sortedTasks} onSubmit={this.onAddTask.bind(this)} />));
+    }
+    else {
+      return (this.wrapPage(<h2>Invalid Tab, choose another</h2>));
+    }
+  }
 }
 
 export default App;
